a0490ee638cc2e9068a75a521a712862b1a523be
tmroz <tom.mroz@d214.org>
2014-05-15 13:14:45 -0500

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Thomas M. Mroz on 5/15/14.
+//  Copyright (c) 2014 BGHS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Thomas M. Mroz on 5/15/14.
+//  Copyright (c) 2014 BGHS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
